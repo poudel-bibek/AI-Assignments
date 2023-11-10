f67424b8ad20413b6428158d10bd7eb8b1f2d2cd
poudel-bibek <serpent.03.meshes@icloud.com>
2023-11-10 16:48:17 -0500

    reduced file

--- Hosted_code/Exploration_Strategies/brain.py
+++ Hosted_code/Exploration_Strategies/brain.py
@@ -10,15 +10,15 @@ torch.backends.cudnn.benchmark = True
 
 
 class Brain:
-    def __init__(self, **config):
+    def __init__(self, current_policy, predictor_model, target_model, **config):
         self.config = config
         self.mini_batch_size = self.config["batch_size"]
         self.device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
         self.obs_shape = self.config["obs_shape"]
 
-        self.current_policy = PolicyModel(self.config["state_shape"], self.config["n_actions"]).to(self.device)
-        self.predictor_model = PredictorModel(self.obs_shape).to(self.device)
-        self.target_model = TargetModel(self.obs_shape).to(self.device)
+        self.current_policy = current_policy #PolicyModel(self.config["state_shape"], self.config["n_actions"]).to(self.device)
+        self.predictor_model = predictor_model #PredictorModel(self.obs_shape).to(self.device)
+        self.target_model = target_model #TargetModel(self.obs_shape).to(self.device)
 
         # print total parameters of each
         params_policy = sum(p.numel() for p in self.current_policy.parameters() if p.requires_grad)
